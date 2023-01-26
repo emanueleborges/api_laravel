4378a747e6e064085223130f58e30b28dc4de49a
Emanuel M Borges <emanuel.eborges@swx-am.net>
2023-01-26 13:12:16 -0400

    first commit

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentsController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/students',[StudentsController::class, 'index']);
+Route::get('/students/{id}',[StudentsController::class, 'show']);
+Route::patch('/students/{id}',[StudentsController::class, 'update']);
+Route::delete('/students/{id}',[StudentsController::class, 'destroy']);
+
+Route::post('/students',[StudentsController::class, 'store']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
